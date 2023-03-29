e8493f84c90086d1acb8f9a88a3a005e0580d511
DiogoVD <diogovdamascena@gmail.com>
2023-03-29 12:02:24 -0300

    Funçoes em javascript e correção dos exercicios anteriores

--- Estudo_javascript/03 - Lista de exercicios/Exercicio2.js
+++ Estudo_javascript/03 - Lista de exercicios/Exercicio2.js
@@ -24,13 +24,13 @@ console.log(imc.toFixed(1));
 if(imc<18.5){
     console.log('Abaixo do peso');
 
-}else if((imc>=18.5)&&(imc>25)){
+}else if((imc>=18.5)&&(imc<25)){
     console.log('Peso normal');
 
-}else if((imc>=25)&&(imc>30)){
+}else if((imc>=25)&&(imc<30)){
     console.log('Acima do peso');
 
-}else if((imc>=30)&&(imc>40)){
+}else if((imc>=30)&&(imc<40)){
     console.log('Obeso');
 
 }else{
---
+++ Estudo_javascript/04 - Funcoes/aula.js
@@ -0,0 +1,13 @@
+
+function imprime(dados){
+    console.log(dados);
+}
+
+function dobro(valor){
+    return valor*2;
+}
+
+imprime('meu nome');
+dobro(10);
+
+imprime(dobro(2));

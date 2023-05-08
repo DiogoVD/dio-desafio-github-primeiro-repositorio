e5802fc50d4421e1f80e2cc0a756b907b78aa3e6
DiogoVD <diogovdamascena@gmail.com>
2023-05-08 18:21:24 -0300

    Entendendo typescript

--- Estudo_Angular/Type_script_angular/compilados/index.js
+++ Estudo_Angular/Type_script_angular/compilados/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 function printaObjeto(pessoa) {
     console.log(pessoa);
 }
@@ -6,3 +15,70 @@ printaObjeto({
     name: "Bruce Wayne",
     vulgo: "Batman"
 });
+let ligado = false;
+let nome = "Diogo";
+let idade = 36;
+let altura = 1.79;
+let nulo = null;
+let indefinido = undefined;
+let retorno;
+let retornoView = false;
+let produto = {
+    name: "Diogo",
+    cidade: "Recife",
+    idade: 36
+};
+let MeuProduto = {
+    nome: "Tenis",
+    preco: 89.90,
+    unidades: 5
+};
+let dados = ["Diogo", "Vieira", "Damascena"];
+let dados2 = ["Diogo", "Vieira", "Damascena"];
+let infos = ["Diogo", 36, 47, "Vieira"];
+let boleto = ["conta de agua", 200, 2222111144443333];
+let aniversario = new Date("2023-05-08 17:30");
+console.log(aniversario.toString());
+function addNumber(x, y) {
+    return x + y;
+}
+function addHello(name) {
+    return `Hello ${name}`;
+}
+let soma = addNumber(3, 7);
+console.log(soma);
+console.log(addHello("Diogo"));
+function CallToPhone(phone) {
+    return phone;
+}
+console.log(CallToPhone("82828288282"));
+function getDataBase(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "Diogo";
+    });
+}
+;
+const bot1 = {
+    id: "1",
+    name: "Megaman"
+};
+const bot2 = {
+    id: "1",
+    name: "Megaman",
+    sayHello: function () {
+        throw new Error("Function not implemented.");
+    }
+};
+console.log(bot1);
+console.log(bot2);
+class Pessoa {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello() {
+        return `hello i'm ${this.name}`;
+    }
+}
+const p = new Pessoa(1, "Diogo");
+console.log(p.sayHello());
--- Estudo_Angular/Type_script_angular/src/index.ts
+++ Estudo_Angular/Type_script_angular/src/index.ts
@@ -8,6 +8,7 @@
 // npx tsc caminho do arquivo 
 // exemplo: npx tsc src\index.ts
 
+
 type heroi = {
     name: string;
     vulgo: string;
@@ -21,3 +22,144 @@ printaObjeto({
     name: "Bruce Wayne",
     vulgo: "Batman"
 })
+
+
+//tipos primitivos: boolean, string, number
+
+let ligado: boolean = false;
+let nome: string = "Diogo";
+let idade: number = 36;
+let altura: number = 1.79;
+
+//tipos especiais: null, undefined
+
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+//tipos abrangentes: any, void
+
+let retorno: void
+let retornoView: any = false;    //usado quando ha algum retorno de função ou variavel
+
+// tipo objeto
+//sem previsibidade
+let produto: object = {
+    name: "Diogo",
+    cidade:"Recife",
+    idade: 36
+};
+
+//objeto tipado com previsibilidade
+type ProdutoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
+};
+
+let MeuProduto: ProdutoLoja = {
+    nome: "Tenis",
+    preco: 89.90,
+    unidades: 5
+};
+
+// Arrays
+
+let dados: string[] = ["Diogo", "Vieira", "Damascena"];
+let dados2: Array<string> = ["Diogo", "Vieira", "Damascena"];
+
+// Arrays multiplos tipos
+
+let infos: (string | number)[] = ["Diogo", 36, 47, "Vieira"];
+
+// Tuplas  - deve-se respeitar a ordem dos tipos das tuplas.
+
+let boleto: [string, number, number] = ["conta de agua", 200, 2222111144443333];
+
+// todos os metodos aplicados no array em javascript é usado em typescript
+
+//Datas
+
+let aniversario: Date = new Date("2023-05-08 17:30");
+console.log(aniversario.toString());
+
+
+//--------------------------------------------------------------------------------
+// Funçoes
+
+function addNumber(x: number, y: number): number{
+    return x + y;
+}
+
+function addHello(name: string){
+    return `Hello ${name}`;
+}
+
+let soma: number = addNumber(3, 7);
+
+console.log(soma);
+console.log(addHello("Diogo"));
+
+// FUnçoes multiplos tipos
+
+function CallToPhone(phone: number | string): number | string{
+    return phone;
+}
+
+console.log(CallToPhone("82828288282"));
+
+// funçoes assincrona
+
+async function getDataBase(id: number): Promise<string | number>{
+    return "Diogo"
+}
+
+//-------------------------------------------------------------------------
+
+// Interfaces (Type X Interfaces)
+
+type robot = {
+    readonly id: number | string;
+    name: string;
+}
+
+interface robot2 {
+    readonly id: number | string;
+    name: string;
+    sayHello(): string;
+};
+
+const bot1: robot = {
+    id: "1",
+    name: "Megaman"
+}
+
+const bot2: robot2 = {
+    id: "1",
+    name: "Megaman",
+    sayHello: function (): string {
+        throw new Error("Function not implemented.");
+    }
+}
+
+console.log(bot1);
+console.log(bot2);
+
+// interfaces
+
+class Pessoa implements robot2 {
+    id: string | number;
+    name: string;
+
+    constructor(id: string | number, name: string){
+        this.id = id;
+        this.name = name;
+    }
+
+    sayHello(): string{
+        return `hello i'm ${this.name}`;
+    }
+}
+
+const p = new Pessoa(1, "Diogo");
+console.log(p.sayHello());
+
